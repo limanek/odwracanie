6e2085a8fb945b05e01d18dd12c61ac6ea4b0e93
Blazej Leman <blazejleman789@gmail.com>
2023-11-20 19:32:23 +0100

    obracanie stringa

--- obr.cpp
+++ obr.cpp
@@ -6,5 +6,5 @@ int main() {
     std::string text;
     std::getline(std::cin, text);
     std::reverse(text.begin(),text.end());
-    std::cout << "odwrucony test: " << text << std::endl;
+    std::cout << "odwrocony test: " << text << std::endl;
 }
